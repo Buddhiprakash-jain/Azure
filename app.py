a6140aa692f2d9de84b3bf12f2fbb09e01cf0983
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2021-06-01 15:38:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Linux World!  <b> Hello buddhijain </b>  <img src=https://mystorage1bp.blob.core.windows.net/myimage/1614800984310.jpg />"
+    return "Linux World!  <b> Hello buddhijain </b>  <img src=https://mystorage1bp.blob.core.windows.net/myimage/1614800984310.jpg width='150' height='200'/>"
